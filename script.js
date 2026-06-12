8bedb22a3a294c68b02de931304e13fe2da8855a
Keerthana <vallurupallikeerthana46@gmail.com>
2026-06-12 11:06:26 +0530

    js

---
+++ script.js
@@ -0,0 +1,49 @@
+//console.log("hello riya");
+//var a=10
+//var b=123.123
+//var c="keerthana"
+//var d=false
+//console.log(a);
+//console.log(b);
+//console.log(c);
+//console.log(d);
+// var a = 10
+// var b = "10"
+// if (a == b) {
+//     console.log("buchi");
+// } else {
+//     console.log("booo");
+// }
+// if (a === b) {
+//     console.log("buchi");
+// } else {
+//     console.log("booo");
+// }
+
+//console.log(2*3);
+//console.log(2 ** 3);
+
+//for(let index = 1; index < 10; index++){
+//  console.log(index);
+//}
+
+// var userInput = 1600;
+// if ((userInput % 4 === 0 && userInput % 100 !== 0) || (userInput % 400 === 0)) {
+//     console.log("hureryyyyyyyyyy its a leap year");
+// } else {
+//     console.log("not a leap year");
+// }
+var num=5
+var fc=0
+for(let i = 1; i <= num; i++) {
+    if(num % i === 0)
+        fc++;
+}
+if(fc === 2) {
+    console.log("prime");
+} else  {
+    console.log("Not a prime");
+}
+
+
+
