cf550c1a244a4fdf28493c8582d1bdd05ea4008c
Keerthana <vallurupallikeerthana46@gmail.com>
2026-06-18 10:51:05 +0530

    added

---
+++ async/index.js
@@ -0,0 +1,15 @@
+const getImage = async() => {
+    let display = document.getElementById("display")
+    
+    try {
+        const res= await fetch("https://dog.ceo/api/breeds/image/random")
+        const data = await res.json()
+
+        display.src = data.message
+
+    } catch (err) {
+        console.log(err.name);
+    }
+    
+
+}
